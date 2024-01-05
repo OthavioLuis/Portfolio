36bde7593b2cf1485e5614cb1066fcb1a461b1d6
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-01-05 17:07:39 -0300

    Otimozação de código

--- javascript.js
+++ javascript.js
@@ -1,3 +1,20 @@
+var bt1 = document.getElementById('bt1');
+var bt2 = document.getElementById('bt2');
+const body = document.getElementById('body');
+
+function dia() {
+    bt1.style.display = 'none';
+    bt2.style.display = 'block';
+    body.style.backgroundImage = "linear-gradient(to bottom, rgb(212, 212, 212), rgb(140, 140, 140))";
+};
+function noite() {
+    bt1.style.display = 'block';
+    bt2.style.display = 'none';
+    body.style.backgroundImage = '';
+};
+
+
+
 function menuAparecer() {
     let menuMobile = document.querySelector('.menu-mobile');
     if (menuMobile.classList.contains('open')) {
@@ -7,4 +24,4 @@ function menuAparecer() {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "img/close_white_36dp.svg";
     }
-}
+};
