b70b9e5bdf51da6397e3cc28388317a508820cc9
OthavioLuis] <othaviorodriguesf@gmail.com>
2024-02-14 23:17:08 -0300

    Criado Function para aparecer as informações criadas em Project

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
 const switcher = document.querySelector('.botao');
+let caixaDisplay = document.querySelector('#boxDisplay');
+let box1 = document.querySelector('.box01');
+let box2 = document.querySelector('.box02');
+let box3 = document.querySelector('.box03');
+let box4 = document.querySelector('.box04');
+let box5 = document.querySelector('.box05');
+let box6 = document.querySelector('.box06');
+let box7 = document.querySelector('.box07');
+let box8 = document.querySelector('.box08');
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('tema-white');
@@ -20,3 +29,48 @@ function menuAparecer() {
         document.querySelector('.icon').src = "img/close_white_36dp.svg";
     }
 };
+
+function sairBox() {
+    caixaDisplay.style.display = 'none'
+    box1.style.display = 'none'
+    box2.style.display = 'none'
+    box3.style.display = 'none'
+    box4.style.display = 'none'
+    box5.style.display = 'none'
+    box6.style.display = 'none'
+    box7.style.display = 'none'
+    box8.style.display = 'none'
+}
+
+function aparecerCaixa1() {
+    caixaDisplay.style.display = 'flex'
+    box1.style.display = 'block'
+}
+function aparecerCaixa2() {
+    caixaDisplay.style.display = 'flex'
+    box2.style.display = 'block'
+}
+function aparecerCaixa3() {
+    caixaDisplay.style.display = 'flex'
+    box3.style.display = 'block'
+}
+function aparecerCaixa4() {
+    caixaDisplay.style.display = 'flex'
+    box4.style.display = 'block'
+}
+function aparecerCaixa5() {
+    caixaDisplay.style.display = 'flex'
+    box5.style.display = 'block'
+}
+function aparecerCaixa6() {
+    caixaDisplay.style.display = 'flex'
+    box6.style.display = 'block'
+}
+function aparecerCaixa7() {
+    caixaDisplay.style.display = 'flex'
+    box7.style.display = 'block'
+}
+function aparecerCaixa8() {
+    caixaDisplay.style.display = 'flex'
+    box8.style.display = 'block'
+}
